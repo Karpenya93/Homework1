06317fa44a59f057742e8ebfb93fc355d69ade2c
Karpenya <karpenyalexa1993@gmail.com>
2022-08-05 02:08:30 +0300

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    Console.Write("Введите первое число ");
+    int firstnumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе  число ");
+    int toonumber = Convert.ToInt32(Console.ReadLine());
+    if (firstnumber > toonumber)
+    {
+        Console.WriteLine($"Число {firstnumber}  больше чем {toonumber}");
+    }
+    else if (firstnumber < toonumber)
+    {
+        Console.WriteLine($"Число {firstnumber}  меньше чем {toonumber}");
+    }
+    else if (firstnumber == toonumber)
+    {
+        Console.WriteLine($"Число {firstnumber}  равно  {toonumber}");
+    }
+}
